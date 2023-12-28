e3e11fdc105fe2a9e4848b80ed7e42d34f3ae44c
hoanh-dev <anhho191297@gmail.com>
2023-12-28 15:52:47 +0700

    fix erro build

--- src/layouts/mainLayout/mainLayout.tsx
+++ src/layouts/mainLayout/mainLayout.tsx
@@ -1,17 +1,14 @@
 import { Footer } from "../footer";
 import { Header } from "../header";
 import { Outlet } from "react-router-dom";
-// import { Header } from "../../components/header";
 
 const MainLayout = () => {
   return (
-    <>
-      <div className="pl-[10%] pr-[10%]">
-        <Header></Header>
-        <Outlet></Outlet>
-        <Footer></Footer>
-      </div>
-    </>
+    <div className="pl-[10%] pr-[10%]">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   );
 };
 
