fa9f859d879b2b7e4386793eec64b64ee7f1008e
hoanh-dev <anhho191297@gmail.com>
2023-12-28 16:41:24 +0700

    update header

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../redux/slices/userSlice";
-import logo from "/images/logo.png";
 import { RootState } from "../../redux/store";
+import Logo from '../../assets/images/svg/logo.svg';
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
   const dispath = useDispatch();
@@ -18,21 +20,56 @@ const Header = () => {
 
   console.log("user", _user);
   return (
-    <>
-      <div>
-        <header className="text-blackColor p-[10px] text-center flex justify-between">
-          <div>
-            <img src={logo} alt="logo"></img>
+    // <>
+    //   <div>
+    //     <header className="text-blackColor p-[10px] text-center flex justify-between">
+    //       <div>
+    //         <img src={Logo} alt="logo"></img>
+    //       </div>
+    //       {/* <div className="flex justify-between w-[20%] pr-10 items-center">
+    //         <h1>Home</h1>
+    //         <h1>Work</h1>
+    //         <h1>Me</h1>
+    //       </div> */}
+    //     </header>
+    //   </div>
+    //   {/* <Outlet></Outlet> */}
+    // </>
+
+    //UPDATE HEADER
+    <header>
+      <div className="container header-wrapper w-full">
+        <div>
+          <div className="m-auto w-20">
+            <img className="sidebar-logo" src={Logo} alt='logo' />
           </div>
-          {/* <div className="flex justify-between w-[20%] pr-10 items-center">
-            <h1>Home</h1>
-            <h1>Work</h1>
-            <h1>Me</h1>
-          </div> */}
-        </header>
+          <div className="text-mainColor text-[18px]" >
+            <ul className="flex items-center gap-5 ">
+              <li>
+                <NavLink to='#'>
+                  <span>Comic List</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='#'>
+                  <span>Category</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='#'>
+                  <span>Popular</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to='#'>
+                  <span>Contact</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      {/* <Outlet></Outlet> */}
-    </>
+    </header>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import './assets/scss/global.scss'
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store.ts";
 import { Provider } from "react-redux";
